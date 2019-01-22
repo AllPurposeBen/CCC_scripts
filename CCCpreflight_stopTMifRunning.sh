a304ca87f289839a818c36ed2a5bc86c05f599cd
Ben Mason <ben@allpurposeben.info>
2019-01-22 14:14:21 -0600

    Add comments and todo

--- CCCpreflight_stopTMifRunning.sh
+++ CCCpreflight_stopTMifRunning.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+### This script will stop an in-progress TM run when CCC job starts
+
 # If TM is running, stop it
 if [[ $(tmutil status | grep Running | xargs) == 'Running = 1;' ]]; then
 	# Is running, stop
@@ -7,5 +9,7 @@ if [[ $(tmutil status | grep Running | xargs) == 'Running = 1;' ]]; then
 	#sleep a few just to give TM a chance to put itself away
 	sleep 30
 fi
+
+## TODO - Disable TM when we start and re-enable when we're done, in case TM starts while we're chooching?
 
 exit 0
